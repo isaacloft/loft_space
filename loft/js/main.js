cf9c4d1f3ffc10a1daa4b5b128ff05bd56249026
Issac Guan <iguan@ajfpartnership.com.au>
2018-03-21 16:53:39 +1100

    custom emoji console output

--- loft/js/main.js
+++ loft/js/main.js
@@ -13,7 +13,7 @@ function init_typing(){
 		el: document.getElementById('block1'),
 		elClass: 'intro',
 		cursorSign: '_',
-		word: 'Hello, welcome to the Loft... ',
+		word: 'Hello, welcome to the Loft...',
 		delay:0,
 		duration: 5,
 		easing: Power4.easeInOut
@@ -32,7 +32,7 @@ function init_rotating(){
 		delay:0,
 		autoPlay: true,
 		rotateWordsOptions : {
-			wordsList : ['human', 'dude', 'my friend'],
+			wordsList : ['humans', 'dudes', 'my friends...<br>'],
 			cycle: false,
 			clear: true
 
@@ -40,6 +40,26 @@ function init_rotating(){
 
 	});
 }
+
+function init_showCase(){
+
+	var last_cursor = $(".gsapCursor");
+	last_cursor.remove();
+
+	var easingPower4 = new gsapTypeIt({
+
+
+		el: document.getElementById('block3'),
+		charterPerSecond: 8,
+		cursorSign: '_',
+		word: 'You are watching demos of front end animation, and the reference can be found in the console..',
+		delay:0,
+		
+	});
+
+}
+
+
 
 function init_hovering(){
 	$cont = $('#block1');
@@ -53,9 +73,26 @@ function intro_Animation(){
 
 	tlMax
 	.to(['#intro_block'], 0, {opacity:1, onComplete:init_typing})
-	.to(['#intro_block'], 0, {opacity:1, onComplete:init_rotating},"+=4.5")
+	.to(['#intro_block'], 0, {opacity:1, onComplete:init_rotating},"+=5.5")
+	.to(['#intro_block'], 0, {opacity:1, onComplete:init_showCase},"+=12")
+	.to(['#intro_block'], 0, {opacity:1, onComplete:typingAnimation_reference},"+=1")
+
+
 
 
 	;
+
+}
+
+
+
+function typingAnimation_reference(){
+	console.log("This typing animation is posted on this discussion forum<br>"); 
+	console.log("https://greensock.com/forums/topic/11361-typewriter-effect/"); 
+	console.log("Of course this is not the only way to animate things like this.."); 
+	console.log("But it's a great extension to the official gsap packages."); 
+	console.beer();
+	console.beer();
+
 
 }
